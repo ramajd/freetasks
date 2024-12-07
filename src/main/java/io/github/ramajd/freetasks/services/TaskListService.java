49f4228a09ca9278d6e1658979cf76acdb1acdb6
Reza Alizadeh Majd <r.a.majd@gmail.com>
2024-12-07 11:39:12 +0330

    delete task list endpoint added

--- src/main/java/io/github/ramajd/freetasks/services/TaskListService.java
+++ src/main/java/io/github/ramajd/freetasks/services/TaskListService.java
@@ -15,4 +15,6 @@ public interface TaskListService {
     Optional<TaskList> getTaskList(UUID id);
 
     TaskList updateTaskList(UUID taskListId, TaskList taskList);
+
+    void deleteTaskList(UUID taskListId);
 }
--- src/main/java/io/github/ramajd/freetasks/services/impl/TaskListServiceImpl.java
+++ src/main/java/io/github/ramajd/freetasks/services/impl/TaskListServiceImpl.java
@@ -70,4 +70,9 @@ public class TaskListServiceImpl implements TaskListService {
         existingTaskList.setUpdated(LocalDateTime.now());
         return taskListRepository.save(existingTaskList);
     }
+
+    @Override
+    public void deleteTaskList(UUID taskListId) {
+        taskListRepository.deleteById(taskListId);
+    }
 }
