c74dcbce0130d6fb3bebd51eff53e673711bf278
Reza Alizadeh Majd <r.a.majd@gmail.com>
2024-12-07 09:11:04 +0330

    initial commit

---
+++ src/main/java/io/github/ramajd/freetasks/FreeTasksApplication.java
@@ -0,0 +1,13 @@
+package io.github.ramajd.freetasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeTasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ramajd/freetasks/FreeTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ramajd.freetasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
